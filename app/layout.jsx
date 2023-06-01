6ade2acb9eed9c5dcae2cd965d3eb0cc47cf5359
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-06-01 20:24:32 +0300

    added globals styles

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "BrainWave",
+  description: " Discover &  Share AI ",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <>
+      <html lang="en">
+        <body>
+          <Provider>
+            <div className="main" v>
+              <div className="gradient"></div>
+            </div>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </body>
+      </html>
+    </>
+  );
+};
+
+export default RootLayout;
