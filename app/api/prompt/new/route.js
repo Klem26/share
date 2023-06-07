7c1edd58de83b5a7f2aaa444258c52d62ac3c754
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-06-07 21:28:18 +0300

    Added models and new page

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,19 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const POST = async (request) => {
+  const { prompt, tag } = await request.json();
+  console.log("post");
+
+  try {
+    await connectToDB();
+
+    const newPrompt = new Prompt({ prompt, tag });
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    console.log("error", error);
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
